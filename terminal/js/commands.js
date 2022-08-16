871f6a2f5a12fd09bd677b869241ec04b4b3e394
AADARSH KUMAR <95774710+Aadarshk01@users.noreply.github.com>
2022-08-16 22:27:09 +0530

    update

---
+++ terminal/js/commands.js
@@ -0,0 +1,70 @@
+var linkedin = "https://www.linkedin.com/in/aadarsh-kumar-ba23b9162/";
+var github = "https://github.com/Aadarshk01";
+var email = 'mailto:aadarshkm01@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Aadarsh Kumar!👋",
+  "I'm a software developer and Full Stack Developer, who builds engaging websites like this one",
+  "After graduating with a Bachelor's of Computer Application, I'm doing my Master's of Computer Application",
+  "as a software engineer building enterprise web applications is my work.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/aadarshkumar' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/aadarshkumar' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  'Opening github         <a href="' + github + '" target="_blank">github/aadarshkumar' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Aadarsh Kumar?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Send an email to Aadarsh Kumar',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">AADARSH KUMAR (AK) Not A Corporation. All knights reserved.</span>',
+
+  "________  _______  ______   _______  _______  _______             _                 _______  _______  _______ ",
+  "(  ___  )(  ___  )(  __  \\ (  ___  )(  ____ )(  ____ \\|\\     /|  | \\    /\\|\\     /|(       )(  ___  )(  ____ )",
+  "| (   ) || (   ) || (  \\  )| (   ) || (    )|| (    \\/| )   ( |  |  \\  / /| )   ( || () () || (   ) || (    )|",
+  "| (___) || (___) || |   ) || (___) || (____)|| (_____ | (___) |  |  (_/ / | |   | || || || || (___) || (____)|",
+  "|  ___  ||  ___  || |   | ||  ___  ||     __)(_____  )|  ___  |  |   _ (  | |   | || |(_)| ||  ___  ||     __)",
+  "| (   ) || (   ) || |   ) || (   ) || (\\ (         ) || (   ) |  |  ( \\ \\ | |   | || |   | || (   ) || (\ (   ",
+  "| )   ( || )   ( || (__/  )| )   ( || ) \\ \\__/\\____) || )   ( |  |  /  \\ \\| (___) || )   ( || )   ( || ) \\ \\__",
+  "|/     \\||/     \\|(______/ |/     \\||/   \\__/\\_______)|/     \\|  |_/    \\/(_______)|/     \\||/     \\||/   \\__/",
+  "                                                                                                        © 2022",
+  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",  
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
